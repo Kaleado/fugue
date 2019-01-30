77f8cc47a7f30f71d7e26b5adf8d4a88864708f8
Kaleado <kaleadoskope@hotmail.com>
2019-01-30 20:46:25 +1100

    Added copy construction and assignment operations.

--- BPlusTree.hpp
+++ BPlusTree.hpp
@@ -17,12 +17,23 @@ namespace Fugue {
 
     template<class Key, int size>
     class BPlusTree {
-    public:
-        void* get(Key& k);
-        void remove(Key& k);
-        void insert(Key& k, void* value);
     private:
         BPlusNode<Key, size>* _root;
+    public:
+
+        void* get(Key& k) {
+            return nullptr;
+        }
+
+        void remove(Key& k) {
+
+        }
+
+        void insert(Key& k, void* value) {
+
+        }
+
+        BPlusTree<Key, size>() = default;
     };
 
     template<class Key, int size>
@@ -30,27 +41,52 @@ namespace Fugue {
     private:
         bool _isLeaf;
         BPlusTree<Key, size>* _tree;
-        void* _value;
-        BPlusNode<Key, size>* _parent = nullptr;
+        BPlusNode<Key, size>* _parent;
         int _currentSize = 0;
         std::array<Key, size> _keys;
         std::array<void*, size+1> _children;
 
         //! Split this node into two, propagating the middle node upwards.
-        void _directInsertKey(Key& k);
+        void _directInsertKey(Key& k) {
+
+        }
+
     public:
         //! Insert a key into this node.
-        void insertKey(Key& k);
+        void insertKey(Key& k) {
+
+        }
+
         //Move assignment/construction.
-        BPlusNode& operator=(const BPlusNode<Key, size>&& rhs);
-        BPlusNode(const BPlusNode<Key, size>&& rhs);
+        BPlusNode<Key, size>& operator=(const BPlusNode<Key, size>&& rhs);
+
+        BPlusNode<Key, size>(const BPlusNode<Key, size>&& rhs);
 
         //Copy assignment/construction.
-        BPlusNode& operator=(const BPlusNode& rhs);
-        BPlusNode(const BPlusNode& rhs);
+        BPlusNode<Key, size>& operator=(const BPlusNode<Key, size>& rhs) {
+            _currentSize = rhs._currentSize;
+            _parent = rhs._parent;
+            _isLeaf = rhs._isLeaf;
+            _children = rhs._children;
+            _keys = rhs._keys;
+            return *this;
+        }
 
-        ~BPlusNode(BPlusTree<Key, size>* tree);
-        ~BPlusNode(BPlusTree<Key, size>* tree, Key& k, void* value);
+        BPlusNode<Key, size>(const BPlusNode<Key, size>& rhs) {
+            _currentSize = rhs._currentSize;
+            _parent = rhs._parent;
+            _isLeaf = rhs._isLeaf;
+            _children = std::move(rhs._children);
+            _keys = std::move(rhs._keys);
+        }
+
+        ~BPlusNode<Key, size>() = default;
+
+        BPlusNode<Key, size>(BPlusTree<Key, size>* tree, bool isLeaf, BPlusNode<Key, size>* parent)
+                : _tree{tree}, _isLeaf{isLeaf}, _parent{parent} {};
+
+        BPlusNode<Key, size>(BPlusTree<Key, size>* tree, bool isLeaf, BPlusNode<Key, size>* parent, std::array<Key, size> keys, std::array<void*, size+1> children)
+                : _tree{tree}, _isLeaf{isLeaf}, _parent{parent}, _keys{keys}, _children{children} {};
     };
 
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "BPlusTree.hpp"
 
 int main() {
+    Fugue::BPlusTree<int, 10> t;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
